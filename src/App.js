3e869c2e3001d07dee2cfb324b3816593b3040e7
EdenGenetTasama <edentasama19@gmail.com>
2022-02-03 19:03:25 +0200

    delete all import not nesscery

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
 import React from "react";
-import Shoes from "./components/ShoesComponent/shoesComponent/shoe";
-import Pants from "./components/PantsConponent/pants";
-import Shirts from "./components/ShirtsComponent/shirt";
 import Routing from "./RoutingPage";
 
 function App() {
   return (
     <div className="App">
-      
       <Routing/>
-        {/* <Shoes />
-
-        <Pants />
-
-
-        <Shirts/> */}
-
     </div>
   );
 }
